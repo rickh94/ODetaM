706ac6fefedabe7c457672817d492ad42e691760
Rick Henry <rickhenry@rickhenry.dev>
2020-12-05 20:05:50 -0500

    add tests for field
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- odetam/field.py
+++ odetam/field.py
@@ -11,35 +11,37 @@ class DetaField:
     def __init__(self, field):
         self.field = field
 
-    def query_expression(self, operator, data):
+    def _query_expression(self, operator, data):
         if isinstance(data, BaseModel):
             data = ujson.loads(data.json())
         return DetaQuery(condition=f"{self.field.name}?{operator}", value=data)
 
     def __eq__(self, other):
-        return self.query_expression("eq", other)
+        return self._query_expression("eq", other)
 
     def __ne__(self, other):
-        return self.query_expression("ne", other)
+        return self._query_expression("ne", other)
 
     def __lt__(self, other):
-        return self.query_expression("lt", other)
+        return self._query_expression("lt", other)
 
     def __gt__(self, other):
-        return self.query_expression("gt", other)
+        return self._query_expression("gt", other)
 
-    def __lte__(self, other):
-        return self.query_expression("lte", other)
+    def __le__(self, other):
+        return self._query_expression("lte", other)
 
-    def __gte__(self, other):
-        return self.query_expression("gte", other)
+    def __ge__(self, other):
+        return self._query_expression("gte", other)
 
     def prefix(self, other):
         if not isinstance(other, str) or self.field.type_ != str:
             raise InvalidDetaQuery("Prefix is only valid for string types")
-        return self.query_expression("pfx", other)
+        return self._query_expression("pfx", other)
 
     def range(self, lower, upper):
+        if self.field.type_ not in (int, float):
+            raise InvalidDetaQuery("Range is only valid for number types")
         if not isinstance(lower, (int, float)):
             raise InvalidDetaQuery("Lower must be a number")
         if not isinstance(upper, (int, float)):
--- tests/test_field.py
+++ tests/test_field.py
@@ -1,70 +1,210 @@
-def test_query_expression():
-    pass
+from typing import List
+from unittest import mock
+
+import pytest
+
+from odetam.exceptions import InvalidDetaQuery
+from odetam.field import DetaField
+from odetam.query import DetaQuery
 
 
-def test_eq():
-    pass
+@pytest.fixture
+def str_field():
+    _mock = mock.MagicMock()
+    _mock.name = "str_field"
+    _mock.type_ = str
+    return DetaField(_mock)
 
 
-def test_ne():
-    pass
+@pytest.fixture
+def int_field():
+    _mock = mock.MagicMock()
+    _mock.name = "int_field"
+    _mock.type_ = int
+    return DetaField(_mock)
 
 
-def test_lt():
-    pass
+@pytest.fixture
+def float_field():
+    _mock = mock.MagicMock()
+    _mock.name = "float_field"
+    _mock.type_ = float
+    return DetaField(_mock)
 
 
-def test_gt():
-    pass
+@pytest.fixture
+def str_list_field():
+    _mock = mock.MagicMock()
+    _mock.name = "str_list_field"
+    _mock.type_ = List[str]
+    return DetaField(_mock)
 
 
-def test_lte():
-    pass
+@pytest.fixture
+def int_list_field():
+    _mock = mock.MagicMock()
+    _mock.name = "int_list_field"
+    _mock.type_ = List[int]
+    return DetaField(_mock)
 
 
-def test_gte():
-    pass
+def test_init():
+    field = DetaField("field")
+    assert field.field == "field"
 
 
-def test_prefix():
-    pass
+def test_query_expression(str_field):
+    qe = str_field._query_expression("eq", "hi")
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "str_field?eq"
+    assert qe.value == "hi"
 
 
-def test_prefix_wrong_type():
-    pass
+def test_eq(str_field):
+    qe = str_field == "test"
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "str_field?eq"
+    assert qe.value == "test"
 
 
-def test_range():
-    pass
+def test_ne(str_field):
+    qe = str_field != "test"
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "str_field?ne"
+    assert qe.value == "test"
 
 
-def test_range_wrong_lower():
-    pass
+def test_lt(int_field):
+    qe = int_field < 10
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "int_field?lt"
+    assert qe.value == 10
 
 
-def test_range_wrong_upper():
-    pass
+def test_gt(int_field):
+    qe = int_field > 10
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "int_field?gt"
+    assert qe.value == 10
 
 
-def test_contains():
-    pass
+def test_le(int_field):
+    qe = int_field <= 10
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "int_field?lte"
+    assert qe.value == 10
 
 
-def test_contains_list():
-    pass
+def test_ge(int_field):
+    qe = int_field >= 10
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "int_field?gte"
+    assert qe.value == 10
 
 
-def test_contains_wrong_type():
-    pass
+def test_prefix(str_field):
+    qe = str_field.prefix("test")
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "str_field?pfx"
+    assert qe.value == "test"
 
 
-def test_not_contains():
-    pass
+def test_prefix_wrong_type(int_field):
+    with pytest.raises(InvalidDetaQuery):
+        int_field.prefix("123")
 
 
-def test_not_contains_list():
-    pass
+def test_prefix_other_wrong_type(str_field):
+    with pytest.raises(InvalidDetaQuery):
+        str_field.prefix(123)
 
 
-def test_not_contains_wrong_type():
-    pass
+def test_range(int_field):
+    qe = int_field.range(1, 5)
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "int_field?r"
+    assert qe.value == [1, 5]
+
+
+def test_range_float(float_field):
+    qe = float_field.range(1.3, 5.8)
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "float_field?r"
+    assert qe.value == [1.3, 5.8]
+
+
+def test_range_wrong_type(str_field):
+    with pytest.raises(InvalidDetaQuery):
+        str_field.range(1, 5)
+
+
+def test_range_wrong_lower(int_field):
+    with pytest.raises(InvalidDetaQuery):
+        int_field.range("1", 5)
+
+
+def test_range_wrong_upper(int_field):
+    with pytest.raises(InvalidDetaQuery):
+        int_field.range(1, "5")
+
+
+def test_range_upper_less_than_lower(int_field):
+    with pytest.raises(InvalidDetaQuery):
+        int_field.range(5, 1)
+
+
+def test_contains(str_field):
+    qe = str_field.contains("test")
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "str_field?contains"
+    assert qe.value == "test"
+
+
+def test_contains_list(str_list_field):
+    qe = str_list_field.contains("test")
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "str_list_field?contains"
+    assert qe.value == "test"
+
+
+def test_contains_wrong_type(int_field):
+    with pytest.raises(InvalidDetaQuery):
+        int_field.contains("test")
+
+
+def test_contains_arg_wrong_type(str_field):
+    with pytest.raises(InvalidDetaQuery):
+        str_field.contains(123)
+
+
+def test_contains_wrong_list_type(int_list_field):
+    with pytest.raises(InvalidDetaQuery):
+        int_list_field.contains("test")
+
+
+def test_not_contains(str_field):
+    qe = str_field.not_contains("test")
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "str_field?not_contains"
+    assert qe.value == "test"
+
+
+def test_not_contains_list(str_list_field):
+    qe = str_list_field.not_contains("test")
+    assert isinstance(qe, DetaQuery)
+    assert qe.condition == "str_list_field?not_contains"
+    assert qe.value == "test"
+
+
+def test_not_contains_wrong_type(int_field):
+    with pytest.raises(InvalidDetaQuery):
+        int_field.not_contains("test")
+
+
+def test_not_contains_wrong_list_type(int_list_field):
+    with pytest.raises(InvalidDetaQuery):
+        int_list_field.not_contains("test")
+
+def test_not_contains_arg_wrong_type(str_field):
+    with pytest.raises(InvalidDetaQuery):
+        str_field.not_contains(123)
